91626041614540487c489116e24ea1313ef85f68
abhayabhinav <abhay.abhinav@gmail.com>
2022-11-14 11:04:41 -0600

    added pythin files to create databse

---
+++ spinupdatabase/createdatabase.py
@@ -0,0 +1,24 @@
+#import the libraries
+import sqlite3
+
+db_conn = sqlite3.connect("airbnblisting.db")
+
+c = db_conn.cursor()
+
+c.execute(
+    """CREATE TABLE sales (
+        id INTEGER,
+        listing_id INTEGER, 
+        listing_name TEXT,
+        listing_location TEXT,
+        listing_neighbourhood TEXT,
+        listing_latitude REAL,
+        listing_longitude REAL,
+        listing_price REAL,
+        listing_review_scores REAL,
+        listing_comments TEXT,
+        PRIMARY KEY(id)
+        );"""
+)
+
+db_conn.close()
