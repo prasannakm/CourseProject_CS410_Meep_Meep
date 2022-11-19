e2e2201ec5116b5a93006ef494d83b01a581758e
jarosales1029 <41701979+jarosales1029@users.noreply.github.com>
2022-11-19 17:38:28 -0600

    Update initial_data_processing.py

--- initial_data_processing.py
+++ initial_data_processing.py
@@ -5,7 +5,7 @@ state = ['CA', 'CA', 'IL', 'TN', 'MA', 'LA', 'TX', 'CO']
 s = 0
 df_main = pd.DataFrame()
 
-# iterate through local file directory to retrieve the required columns for merging
+# iterate and read listings local file directory/keep the required columns for merging
 for city in cities:
     listings = pd.read_csv(f'{city}/listings.csv')
     listings = listings.loc[:, ['id',
@@ -17,10 +17,11 @@ for city in cities:
                          'price',
                          'review_scores_value']]
     
+    # read reviews local file directory/keep the required columns for merging
     reviews = pd.read_csv(f'{city}/reviews.csv')
     reviews = reviews.loc[:, ['listing_id', 'comments']]
 
-    # merge the listing and reviews datasets and retrieve only the required columns for our use case
+    # merge the listing and reviews datasets and keep only the required columns for our use case
     df = listings.merge(reviews, left_on='id', right_on='listing_id')
     df = df.loc[:, ['id',
                     'name',
@@ -39,18 +40,20 @@ for city in cities:
     # remove html tags from the comments
     df['comments'] = df['comments'].str.replace(r'<[^<>]*>', '', regex=True)
     
-    # take 10 random samples for each listing and drop any rows with duplicate comments
+    # drop any rows with duplicate comments
     df = df.drop_duplicates(['comments'])
+    
+    # take 10 random samples for each listing
     df = df.groupby('id').sample(10, replace=True)
         
     # rename host_location to location & from identified list variables, change the value to city, state
     df = df.rename(columns={'host_location':'location'})
     df['location'] = f'{city}, {state[s]}'
     
-    # merging the files
+    # merging files
     df_main = df_main.append(df)
     
-    # increase s to iterate through given city, state lists
+    # increment s to iterate through given city/state lists
     s += 1
     
 # print final dataset
