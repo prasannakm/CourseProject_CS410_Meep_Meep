48a3161fd73412501e0650ac8ca5f39eca47f258
jarosales1029 <41701979+jarosales1029@users.noreply.github.com>
2022-11-20 12:35:22 -0600

    Update 1_Data_Processing_Cleaning_Analysis.py

--- 1_Data_Processing_Cleaning_Analysis.py
+++ 1_Data_Processing_Cleaning_Analysis.py
@@ -1,4 +1,28 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import nltk
+from nltk.tokenize import word_tokenize 
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+stop = stopwords.words('english')
+from nltk.stem import PorterStemmer
+stemmer = PorterStemmer()
+nltk.download('vader_lexicon')
+nltk.download('punkt')
+
+from nltk.sentiment.vader import SentimentIntensityAnalyzer as vad
+
+from sklearn import feature_extraction
+from sklearn.metrics import confusion_matrix
+from sklearn import model_selection as ms
+from sklearn import naive_bayes
+from sklearn import preprocessing
+from sklearn.metrics import confusion_matrix
+import warnings
+warnings.filterwarnings("ignore")
+
 
 cities = ['Los Angeles', 'San Francisco', 'Chicago', 'Nashville', 'Boston', 'New Orleans', 'Austin', 'Denver']
 state = ['CA', 'CA', 'IL', 'TN', 'MA', 'LA', 'TX', 'CO']
@@ -33,8 +57,8 @@ for city in cities:
                     'review_scores_value',
                     'comments']]
     
-    # rename host_location to location & from identified list variables, change the value to city, state
-    df = df.rename(columns={'host_location':'location'})
+    # rename columns & from identified list variables, change the value to city, state
+    df = df.rename(columns={'host_location':'location', 'review_scores_value':'review_scores'})
     df['location'] = f'{city}, {state[s]}'
     
     # remove non-ASCII characters
@@ -59,6 +83,22 @@ for city in cities:
     # take 10 random samples for each listing
     df = df.groupby('id').sample(10, replace=True)
     
+    # drop any rows with duplicate comments
+    df = df.drop_duplicates(['comments'])
+    
+    # drop rows that are missing values
+    df = df.dropna()
+    
+    # convert the comment values to string and lowercase
+    df['comments'] = df['comments'].astype(str)
+    df['comments'] = df['comments'].str.lower()
+
+    # tokenize the reviews to word token
+    df['comments'] = df['comments'].apply(word_tokenize)
+
+    # remove stop words from the tokens
+    df['comments'] = df['comments'].apply(lambda x: ' '.join([item for item in x if item not in stop]))
+    
     # merging files
     df_main = df_main.append(df)
     
@@ -66,4 +106,45 @@ for city in cities:
     s += 1
     
 # print final dataset
-df1 = df_main.to_csv('Data_Final_10.csv', sep=',', index=False)
+#df1 = df_main.to_csv('Dataset_Final.csv', sep=',', index=False)
+
+# Vader Sentiment Analysis
+# making additional columns for sentiment score in the vader dataframe
+sentiment = vad()
+
+sen = ['Positive', 'Negative', 'Neutral']
+sentiments = [sentiment.polarity_scores(i) for i in df_main['comments'].values]
+df_main['Vad_Negative Score'] = [i['neg'] for i in sentiments]
+df_main['Vad_Positive Score'] = [i['pos'] for i in sentiments]
+df_main['Vad_Neutral Score'] = [i['neu'] for i in sentiments]
+df_main['Vad_Compound Score'] = [i['compound'] for i in sentiments]
+score = df_main['Vad_Compound Score'].values
+t = []
+
+for i in score:
+    if i >= 0.05:
+        t.append('Positive')
+    elif i <= -0.05:
+        t.append('Negative')
+    else:
+        t.append('Neutral')
+        
+df_main['Vad_Overall_Sentiment'] = t
+
+# Vader Visualizations
+'''
+sns.countplot(df_main['Vad_Overall_Sentiment'])
+explode = [0, 0.1, 0.1]
+df_main["Vad_Overall_Sentiment"].value_counts().plot.pie(title="Over All Sentiment",autopct='%1.1f%%', 
+                        explode = explode
+                                 )
+time_df = df_main.groupby(['Vad_Overall_Sentiment','location'])['comments'].count().reset_index()
+plt.figure(figsize=(30, 8))
+sns.barplot(data=time_df, x='location',y='comments', hue='Vad_Overall_Sentiment')
+plt.show()
+
+time_df = df_main.groupby(['Vad_Overall_Sentiment','review_scores'])['comments'].count().reset_index()
+plt.figure(figsize=(30,8))
+sns.barplot(data=time_df, x='review_scores', y='comments', hue='Vad_Overall_Sentiment')
+plt.show()
+'''
